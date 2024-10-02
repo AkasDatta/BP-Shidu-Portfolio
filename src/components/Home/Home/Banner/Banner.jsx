c6d9c2705d132aac7771736fb73814b3ef1364e9
Akas_Datta <012akasdatta@gmail.com>
2024-10-02 23:06:45 +0600

    routes file added

---
+++ src/components/Home/Home/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+
+const Banner = () => {
+    return (
+        <div>
+            <h2>
+                jjjj
+
+            </h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Home/Shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div>
+             <p className="flex justify-center text-[#bdbcbc] mt-6">© Inspired by <a className="mx-1 text-teal-500" href="https://wpriverthemes.com/gridx/" target="_blank" rel="noopener noreferrer">GridX</a></p>
+        </div>
+    );
+};
+
+export default Footer;
