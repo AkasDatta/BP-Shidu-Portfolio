1fb43c22822a82e6fa1ab6e2bf370137a2b52cb3
Akas_Datta <012akasdatta@gmail.com>
2024-10-05 00:33:03 +0600

    modifyed text

--- src/components/Home/Home/Banner/Banner.jsx
+++ src/components/Home/Home/Banner/Banner.jsx
@@ -26,8 +26,8 @@ const Banner = () => {
                             <img src={profile} alt="" className="border-4 border-gray-500 rounded-md"/>
                         </div>
                         <div>
-                            <h2 className="text-3xl font-bold text-[#C4CFDE] py-4">Badhon Paul Shidu</h2>
-                            <p className="font-semibold text-[#838994] text-lg">I am a Network Engineer</p>
+                            <h2 className="text-4xl font-bold text-[#C4CFDE] py-4">Badhon Paul Shidu</h2>
+                            <p className="font-semibold text-[#838994] text-xl">I am a Network Engineer</p>
                             <div className="flex gap-5 pt-12">
                                 <div className="p-2 bg-[#15171a] text-gray-300 rounded-lg hover:bg-black cursor-pointer">
                                     <FaFacebook className="text-4xl"/>
@@ -45,21 +45,21 @@ const Banner = () => {
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-5 my-0 mb-5 md:my-5 lg:my-5">
                             <div className="pb-4">
                                 <p className="font-semibold text-[#838994] text-lg py-2">EMAIL</p>
-                                <h2 className="font-semibold text-[#C4CFDE] text-xl">bachon@gmail.com</h2>
+                                <h2 className="font-semibold text-[#C4CFDE] text-2xl">bachon.paul @gmail.com</h2>
                             </div>
                             <div>
                                 <p className="font-semibold text-[#838994] text-lg py-2">PHONE</p>
-                                <h2 className="font-semibold text-[#C4CFDE] text-xl">+880 1223244556</h2>
+                                <h2 className="font-semibold text-[#C4CFDE] text-2xl">+880 1223244556</h2>
                             </div>
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 mx-5 my-0 mb-5 md:my-5 lg:my-5">
                             <div className="pb-6">
                                 <p className="font-semibold text-[#838994] text-lg py-2">BIRTHDAY</p>
-                                <h2 className="font-semibold text-[#C4CFDE] text-xl">20 August</h2>
+                                <h2 className="font-semibold text-[#C4CFDE] text-2xl">20 August</h2>
                             </div>
                             <div>
                                 <p className="font-semibold text-[#838994] text-lg py-2">LOCATION</p>
-                                <h2 className="font-semibold text-[#C4CFDE] text-xl">Dhaka, Bangladesh</h2>
+                                <h2 className="font-semibold text-[#C4CFDE] text-2xl">Dhaka, Bangladesh</h2>
                             </div>
                         </div>
                     </div>
--- src/components/Home/Shared/Footer/Footer.jsx
+++ src/components/Home/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div>
-             <p className="flex justify-center text-[#bdbcbc] mt-6">© Inspired by <a className="mx-1 text-teal-500" href="https://wpriverthemes.com/gridx/" target="_blank" rel="noopener noreferrer">GridX</a></p>
+             <p className="flex justify-center text-[#bdbcbc] py-6">© Inspired by <a className="mx-1 text-teal-500" href="https://wpriverthemes.com/gridx/" target="_blank" rel="noopener noreferrer">GridX</a></p>
         </div>
     );
 };
