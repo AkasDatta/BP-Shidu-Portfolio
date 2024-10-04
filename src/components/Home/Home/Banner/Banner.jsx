d34f22d4af1bf541a722929dd3d0b7e04774f6c6
Akas_Datta <012akasdatta@gmail.com>
2024-10-05 00:55:25 +0600

    added card file

--- src/components/Home/Home/Banner/Banner.jsx
+++ src/components/Home/Home/Banner/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
                 className="p-5 bg-image"
                 style={{
                     backgroundImage:
-                    "url('https://images.unsplash.com/photo-1621419203051-f4e463849784?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
+                    "url('https://images.unsplash.com/photo-1510218830377-2e994ea9087d?q=80&w=1616&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
                     height: '300px',
                 }}
                 ></div>
---
+++ src/components/Home/Home/Card/Card.jsx
@@ -0,0 +1,15 @@
+
+const Card = () => {
+    return (
+        <div className="mx-10 my-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-5">
+            <div className="bg-[#202327] rounded-lg p-5 shadow-lg">
+                <button className="">Blog</button>
+            </div>
+            <div className="bg-[#202327] col-span-1 md:col-span-3 lg:col-span-4">
+                hh
+            </div>
+        </div>
+    );
+};
+
+export default Card;
