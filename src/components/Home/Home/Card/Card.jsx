9854264b086ccd4effd59838277d7b5d1be0a7a2
Akas_Datta <012akasdatta@gmail.com>
2024-10-05 01:14:24 +0600

    asdded blog

--- src/components/Home/Home/Card/Card.jsx
+++ src/components/Home/Home/Card/Card.jsx
@@ -1,12 +1,17 @@
 
 const Card = () => {
     return (
-        <div className="mx-10 my-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-5">
-            <div className="bg-[#202327] rounded-lg p-5 shadow-lg">
-                <button className="">Blog</button>
+        <div className="mx-10 my-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-6">
+            <div className="bg-[#202327] rounded-lg p-5 shadow-lg  flex-col gap-6">
+                <button className="bg-[#FF014F] text-[#C4CFDE] w-full py-9 rounded-lg font-semibold text-xl">Blog</button>
+                <button className="hover:bg-[#FF014F] text-[#C4CFDE] w-full py-9 rounded-lg font-semibold text-xl">About</button>
+                <button className="hover:bg-[#FF014F] text-[#C4CFDE] w-full py-9 rounded-lg font-semibold text-xl">Work</button>
+                <button className="hover:bg-[#FF014F] text-[#C4CFDE] w-full py-9 rounded-lg font-semibold text-xl">Resume</button>
+                <button className="hover:bg-[#FF014F] text-[#C4CFDE] w-full py-9 rounded-lg font-semibold text-xl">Contact</button>
             </div>
-            <div className="bg-[#202327] col-span-1 md:col-span-3 lg:col-span-4">
-                hh
+
+            <div className="bg-[#202327] rounded-lg p-5 shadow-lg col-span-1 md:col-span-3 lg:col-span-5">
+                hh 
             </div>
         </div>
     );
