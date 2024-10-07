9b22daa8b8d0900d8d4a5c768f8e9d1a07c3b2dd
Akas_Datta <012akasdatta@gmail.com>
2024-10-08 01:26:29 +0600

    added text and description

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,8 +4,6 @@ import {
 import Main from "../Layout/Main";
 import Home from "../components/Home/Home/Home";
 import BlogDetails from "../components/Home/Home/CardDetails/Blog/BlogDetails";
-import Blog from "../components/Home/Home/CardDetails/Blog/Blog";
-
 
 export const router = createBrowserRouter([
     {
