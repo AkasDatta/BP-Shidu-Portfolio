276347262916f32a0cf4c06dff04d810867ccdc2
Akas_Datta <012akasdatta@gmail.com>
2024-10-08 00:59:36 +0600

    added blogdetails git push

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -15,7 +15,7 @@ const Main = () => {
     return (
           <div>
             {isVisible ? <div className="flex justify-center items-center h-screen">
-                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-blue-500"></div>
+                <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-red-500"></div>
                 </div> : <div className="bg-[#24272B]">
                         <Outlet></Outlet>
                         <Footer></Footer>
--- src/components/Home/Home/CardDetails/Blog/BlogDetails.jsx
+++ src/components/Home/Home/CardDetails/Blog/BlogDetails.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const BlogDetails = () => {
     const { id } = useParams(); // Get the blog ID from the URL
@@ -18,9 +19,30 @@ const BlogDetails = () => {
     if (!blog) return <div>Loading...</div>;
 
     return (
-        <div>
-            <img src={blog.image2} alt={blog.title} />
+        <div className="max-w-screen-xl py-12 mx-auto px-5 lg:px-12 rounded-sm">
+            <article className="group">
+                <div className="flex justify-between items-center  pb-5 ">
+                    <h1 className="text-4xl font-bold text-gray-300">{blog.title}</h1>
+                    <button className="bg-gray-900 hover:bg-black p-2 rounded-xl"><IoMdClose className="text-[#ff014e] text-2xl "/></button>
+                </div>
+                <img src={blog.image} className="h-full w-full rounded-lg object-cover shadow-xl transition group-hover:grayscale-[50%]" alt={blog.title} />
+
+                <div className="p-4">
+                    <a href="#">
+                    <h3 className="text-lg font-medium text-gray-900">Finding the Journey to Mordor</h3>
+                    </a>
+
+                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
+                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
+                    dignissimos. Molestias explicabo corporis voluptatem?
+                    </p>
+                </div>
+            </article>
+            <img src={blog.image} alt={blog.title} />
             <h1>{blog.title}</h1>
+            <img src={blog.image2} alt={blog.title} />
             <p>{blog.description}</p>
             <p>{blog.date}</p>
             <p>{blog.readTime}</p>
