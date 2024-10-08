b1e0a07db8b3a0229460d18ad4269700ed1ebcb7
Akas_Datta <012akasdatta@gmail.com>
2024-10-09 00:57:56 +0600

    name phone number and email done

--- src/components/Home/Home/CardDetails/Contact/Contact.jsx
+++ src/components/Home/Home/CardDetails/Contact/Contact.jsx
@@ -22,9 +22,19 @@ const Contact = () => {
                 </div>
                 <div className="col-span-1 lg:col-span-4">
                     <form className="mx-auto bg-[#212428] p-5 rounded-xl shadow-2xl">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mb-5">
+                        <div>
+                            <label for="name" className="block mb-3 text-sm text-gray-300">YOUR NAME</label>
+                            <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Name" required />
+                        </div>
+                        <div>
+                            <label for="number" className="block mb-3 text-sm text-gray-300">PHONE NUMBER</label>
+                            <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Number" required />
+                        </div>
+                    </div>
                     <div className="mb-5">
-                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                        <label for="email" className="block mb-3 text-sm text-gray-300">EMAIL</label>
+                        <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="example@email.com" required />
                     </div>
                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
                     </form>
