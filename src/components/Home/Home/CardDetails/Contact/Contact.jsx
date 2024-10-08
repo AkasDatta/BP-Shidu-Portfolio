788369980188be00a49066b4b3381e0e28d4a4f7
Akas_Datta <012akasdatta@gmail.com>
2024-10-09 00:48:18 +0600

    added input form

--- src/components/Home/Home/CardDetails/Contact/Contact.jsx
+++ src/components/Home/Home/CardDetails/Contact/Contact.jsx
@@ -21,9 +21,14 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="col-span-1 lg:col-span-4">
-                    <h2 className="text-[#ff014e] font-semibold text-lg pt-2">VISIT MY PORTFOLIO & HIRE ME</h2>
-                    <h1 className="text-3xl font-bold text-gray-300 pt-4 pb-6">About Me</h1>
-                    <p className="text-lg text-gray-400 ">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel minus labore hic magni quas placeat non et ut dolorum numquam, repellendus soluta accusamus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, aut!</p>
+                    <form className="mx-auto bg-[#212428] p-5 rounded-xl shadow-2xl">
+                    <div className="mb-5">
+                        <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                    </div>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+                    </form>
+
                 </div>
             </div>
         </div>
