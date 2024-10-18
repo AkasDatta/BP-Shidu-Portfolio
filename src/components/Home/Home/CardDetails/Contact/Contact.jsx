91234ce7c7b74d7cdd4803f3ddab7fdc5bedb16a
Akas_Datta <012akasdatta@gmail.com>
2024-10-18 23:49:33 +0600

    install email js

--- src/components/Home/Home/CardDetails/Contact/Contact.jsx
+++ src/components/Home/Home/CardDetails/Contact/Contact.jsx
@@ -21,30 +21,30 @@ const Contact = () => {
                     </div>
                 </div>
                 <div className="col-span-1 lg:col-span-4">
-                    <form className="mx-auto bg-[#212428] p-5 rounded-xl shadow-2xl">
+                    <form ref={form} onSubmit={sendEmail} className="mx-auto bg-[#212428] p-5 rounded-xl shadow-2xl">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5 mb-7">
                         <div>
                             <label for="text" className="block mb-3 text-sm text-gray-300">YOUR NAME</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Name" required />
+                            <input name="from_name" type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Name" required />
                         </div>
                         <div>
                             <label for="number" className="block mb-3 text-sm text-gray-300">PHONE NUMBER</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Number" required />
+                            <input name="message" type="phone" id="phone" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Number" required />
                         </div>
                     </div>
                     <div className="mb-7">
                         <label for="email" className="block mb-3 text-sm text-gray-300">EMAIL</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="example@email.com" required />
+                        <input name="email_id" type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="example@email.com" required />
                     </div>
                     <div className="mb-7">
                         <label for="text" className="block mb-3 text-sm text-gray-300">SUBJECT</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="" required />
+                        <input name="message" type="email" id="email" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="" required />
                     </div>
                     <div className="mb-7">
                         <label for="message" className="block mb-3 text-sm text-gray-300">YOUR MESSAGE</label>
-                        <textarea id="message" rows="11" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Write your thoughts here..."></textarea>
+                        <textarea name="message" id="message" rows="11" className="shadow-sm bg-gray-800 border border-gray-600 text-white text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Write your thoughts here..."></textarea>
                     </div>
-                    <button type="submit" className="text-white bg-[#FF014F] hover:bg-[#ce184e] duration-500 focus:ring-4 focus:outline-none focus:ring-[#9e123c] font-medium rounded-lg text-sm mx-auto w-full py-2.5 text-center">SEND MESSAGE</button>
+                    <button type="submit" value="sendemai" className="text-white bg-[#FF014F] hover:bg-[#ce184e] duration-500 focus:ring-4 focus:outline-none focus:ring-[#9e123c] font-medium rounded-lg text-sm mx-auto w-full py-2.5 text-center">SEND MESSAGE</button>
                     </form>
 
                 </div>
