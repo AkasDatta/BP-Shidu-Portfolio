218b2e3d64c74b43b5d94a4b68a468fc358ad191
Akas Datta <68992911+AkasDatta@users.noreply.github.com>
2024-10-25 23:52:14 +0600

    Update Footer.jsx

--- src/components/Home/Shared/Footer/Footer.jsx
+++ src/components/Home/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <div>
-             <p className="flex justify-center text-[#bdbcbc] pt-6">© Inspired by <a className="mx-1 text-[#FF014F]" href="https://themeforest.net/user/rainbow-themes" target="_blank" rel="noopener noreferrer">Rainbow-Themes</a></p>
+             <p className="flex justify-center text-[#bdbcbc] pb-6 pt-6">© Inspired by <a className="mx-1 text-[#FF014F]" href="https://themeforest.net/user/rainbow-themes" target="_blank" rel="noopener noreferrer">Rainbow-Themes</a></p>
         </div>
     );
 };
