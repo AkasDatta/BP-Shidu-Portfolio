05a0d794f620d948a416ab90e6373a39b9881cab
Akas_Datta <012akasdatta@gmail.com>
2024-11-04 23:19:54 +0600

    added required

--- src/components/Home/login/Register/Register.jsx
+++ src/components/Home/login/Register/Register.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Register = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
-    
+
     return (
         <div>
              <section className="bg-gray-50 dark:bg-[#24272B]">
@@ -14,14 +14,29 @@ const Register = () => {
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Create an account
                             </h1>
-                            <form onSubmit={handleSignup} className="space-y-4 md:space-y-6">
+                            <form onSubmit={handleSubmit(onSubmit)}className="space-y-4 md:space-y-6">
+                            <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Your Name
+                                    </label>
+                                    <input
+                                        type="name"
+                                        name="name"
+                                        id="name"
+                                        {...register("name")}
+                                        placeholder="Name"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                    />
+                                </div>
                                 <div>
                                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         Your email
                                     </label>
                                     <input
                                         type="email"
-                                        name="email"
+                                        name="email" 
+                                        {...register("email")}
                                         id="email"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         placeholder="name@gmail.com"
@@ -36,19 +51,7 @@ const Register = () => {
                                         type="password"
                                         name="password"
                                         id="password"
-                                        placeholder="••••••••"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        required
-                                    />
-                                </div>
-                                <div>
-                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                        Confirm password
-                                    </label>
-                                    <input
-                                        type="password"
-                                        name="password"
-                                        id="password"
+                                        {...register("password")}
                                         placeholder="••••••••"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required
