3d9da8f90e4fbf41d05d31acf13985b2580fe509
Akas_Datta <012akasdatta@gmail.com>
2024-11-20 00:09:38 +0600

    added secret things text

--- src/components/Home/Shared/Secret/Secret.jsx
+++ src/components/Home/Shared/Secret/Secret.jsx
@@ -3,6 +3,9 @@ const Secret = () => {
     return (    
         <div className="flex justify-center items-center h-screen">
             <div className="text-white ">Secret Things</div>
+            <div>
+                <h2>Here is the secret things</h2>
+            </div>
         </div>
     );
 };
