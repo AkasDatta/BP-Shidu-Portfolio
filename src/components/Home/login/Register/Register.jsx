604d62bf423914252f47ccd17002acbf091fc575
Akas_Datta <012akasdatta@gmail.com>
2024-11-20 00:48:05 +0600

    some error solved

--- src/components/Home/login/Register/Register.jsx
+++ src/components/Home/login/Register/Register.jsx
@@ -24,19 +24,41 @@ const Register = () => {
                                 Create an account
                             </h1>
                             <form onSubmit={handleSubmit(onSubmit)}className="space-y-4 md:space-y-6">
-                            <div>
+                                <div className='form-control'>
                                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         Your Name
                                     </label>
                                     <input
-                                        type="name"
-                                        name="name"
-                                        id="name"
-                                        {...register("name")}
+                                        type="text"
+                                        name='name'
+                                        {...register("name", {
+                                            required: true
+                                        })}
                                         placeholder="Name"
                                         className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required
                                     />
+                                      {
+                                        errors.photoURL && <span className='text-red-600'>Photo URL is required</span>
+                                    }
+                                </div>
+                                <div>
+                                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Photo URL
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="photoURL" 
+                                        {...register("photoURL", {
+                                            required: true
+                                        })}
+                                        id="photoURL"
+                                        placeholder="Photo URL"
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    />
+                                    {
+                                        errors.photoURL && <span className='text-red-600'>Photo URL is required</span>
+                                    }
                                 </div>
                                 <div>
                                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -58,8 +80,6 @@ const Register = () => {
                                     </label>
                                     <input
                                         type="password"
-                                        name="password"
-                                        id="password"
                                         {...register("password", {
                                             required: true,
                                             minLength: 6,
