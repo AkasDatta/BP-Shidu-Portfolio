9d5c11b94af58cf04a725beef9bf26c91c8cf635
Akas_Datta <012akasdatta@gmail.com>
2024-12-25 23:16:04 +0600

    comment

--- src/components/Home/login/Login/Login.jsx
+++ src/components/Home/login/Login/Login.jsx
@@ -16,12 +16,12 @@ const Login = () => {
         const form = event.target;
         const email = form.elements.email.value;
         const password = form.elements.password.value;
-        console.log(email, password);
-        
+        // console.log(email, password);
+
         signIn(email, password)
         .then(result => {
             const user = result.user;
-            console.log(user);
+            // console.log(user);
             Swal.fire("User login successfully");
         });
         navigate(from, {replace: true });
