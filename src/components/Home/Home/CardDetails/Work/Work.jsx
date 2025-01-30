ecb5b57b945f6d128f9b732570fe580b2af5a7ca
Akas_Datta <012akasdatta@gmail.com>
2025-01-30 23:53:52 +0600

    added grid item on the work position

--- src/components/Home/Home/CardDetails/Work/Work.jsx
+++ src/components/Home/Home/CardDetails/Work/Work.jsx
@@ -4,8 +4,8 @@ import network from "../../../../../assets/network.jpg";
 const Work = () => {
     return (
         <div>
-              <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6">
+              <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 grid grid-cols-2 gap-8 w-full">
                         <div className="h-48 sm:h-80 w-full overflow-hidden rounded-2xl">
                             <img
                                 src={image}
@@ -35,6 +35,9 @@ const Work = () => {
                             <h2 className="text-gray-400 font-semibold text-3xl py-4 hover:text-[#FF014F] duration-500">This Site Describe about my Skill and Abilities and also my Daily activities.</h2>
                         </div>
                     </a>
+            </div>
+            <div>
+                <h2>hh</h2>
             </div>
         </div>
     );
