a04eec89b740a5a89b96ae2cc5808889ad373c36
Akas_Datta <012akasdatta@gmail.com>
2025-01-31 00:17:30 +0600

    added responsive

--- src/components/Home/Home/CardDetails/Work/Work.jsx
+++ src/components/Home/Home/CardDetails/Work/Work.jsx
@@ -5,30 +5,30 @@ const Work = () => {
     return (
         <div>
               <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 flex gap-8">
-                        <div className="h-56 overflow-hidden rounded-2xl">
+                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 grid grid-cols-1 lg:grid-cols-5 gap-8">
+                        <div className="h-56 overflow-hidden rounded-2xl col-span-1 lg:col-span-2">
                             <img
                                 src={image}
                                 alt="Blog"
-                                className="h-56 object-cover transform transition-transform duration-300 hover:scale-105"
+                                className="h-56 w-full object-cover transform transition-transform duration-300 hover:scale-105"
                             />
                         </div>
-                        <div className="relative py-6">
+                        <div className="col-span-1 lg:col-span-3 py-6">
                             <div className="flex justify-between items-center">
                                 <p className="text-[#FF014F] font-bold">Badhon Paul Shidu</p>
                             </div>
                             <h2 className="text-gray-400 font-semibold text-3xl py-4 hover:text-[#FF014F] duration-500">My personal blog</h2>
                         </div>
                     </a>
-                    <a href="https://shidupaul.blogspot.com/" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 flex gap-8">
-                        <div className="h-56 overflow-hidden rounded-2xl">
+                    <a href="https://shidupaul.blogspot.com/" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 grid grid-cols-1 lg:grid-cols-5 gap-8">
+                        <div className="h-56 overflow-hidden rounded-2xl col-span-1 lg:col-span-2">
                             <img
                                 src={network}
                                 alt="Blog"
                                 className="h-56 w-full object-cover transform transition-transform duration-300 hover:scale-105"
                             />
                         </div>
-                        <div className="relative py-6">
+                        <div className=" py-6 col-span-1 lg:col-span-3">
                             <div className="flex justify-between items-center">
                                 <p className="text-[#FF014F] font-bold">Shidu's flatform</p>
                             </div>
