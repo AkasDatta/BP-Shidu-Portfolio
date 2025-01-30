612d8032c4e79aa221db477142bc8f67a0f87c87
Akas_Datta <012akasdatta@gmail.com>
2025-01-31 00:02:35 +0600

    added flex item

--- src/components/Home/Home/CardDetails/Work/Work.jsx
+++ src/components/Home/Home/CardDetails/Work/Work.jsx
@@ -5,12 +5,12 @@ const Work = () => {
     return (
         <div>
               <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
-                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 grid grid-cols-2 gap-8 w-full">
-                        <div className="h-48 sm:h-80 w-full overflow-hidden rounded-2xl">
+                    <a href="https://www.blogger.com/profile/14962074601999675358" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 flex gap-8">
+                        <div className="h-56 overflow-hidden rounded-2xl">
                             <img
                                 src={image}
                                 alt="Blog"
-                                className="h-full w-full object-cover transform transition-transform duration-300 hover:scale-105"
+                                className="h-56 object-cover transform transition-transform duration-300 hover:scale-105"
                             />
                         </div>
                         <div className="relative py-6">
@@ -20,19 +20,19 @@ const Work = () => {
                             <h2 className="text-gray-400 font-semibold text-3xl py-4 hover:text-[#FF014F] duration-500">My personal blog</h2>
                         </div>
                     </a>
-                    <a href="https://shidupaul.blogspot.com/" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6">
-                        <div className="h-48 sm:h-80 w-full overflow-hidden rounded-2xl">
+                    <a href="https://shidupaul.blogspot.com/" className="rounded-2xl bg-[#1a1d20] duration-500 hover:bg-[#121416] p-6 flex gap-8">
+                        <div className="h-56 overflow-hidden rounded-2xl">
                             <img
                                 src={network}
                                 alt="Blog"
-                                className="h-full w-full object-cover transform transition-transform duration-300 hover:scale-105"
+                                className="h-56 w-full object-cover transform transition-transform duration-300 hover:scale-105"
                             />
                         </div>
                         <div className="relative py-6">
                             <div className="flex justify-between items-center">
                                 <p className="text-[#FF014F] font-bold">Shidu's flatform</p>
                             </div>
-                            <h2 className="text-gray-400 font-semibold text-3xl py-4 hover:text-[#FF014F] duration-500">This Site Describe about my Skill and Abilities and also my Daily activities.</h2>
+                            <h2 className="text-gray-400 font-semibold text-3xl py-4 hover:text-[#FF014F] duration-500">This Site Describe about my Skill and my Daily activities.</h2>
                         </div>
                     </a>
             </div>
